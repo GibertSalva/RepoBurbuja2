fd63bdb16dc5663db4f4de673ca50032f1a0c70d
Lucio Morales <lucio.moralesdemaria@gmail.com>
2021-06-06 17:28:01 -0300

    Agregado de alarmas al model y admin

--- FocusBV/models.py
+++ FocusBV/models.py
@@ -1,15 +1,15 @@
 from django.db import models
 from django.db.models.fields import BooleanField, CharField, IntegerField
-from django.forms import ModelForm
+# from django.forms import ModelForm
 
 
 # Create your models here.
-#TODO agregar sentido de la ruta y entre calles
+# TODO agregar sentido de la ruta y entre calles
 
 class FormPadre(models.Model):
     
     idAlarma = models.CharField(max_length=50, primary_key=True)
-    tipo = models.CharField(max_length=30)
+    tipoSiniestro = models.CharField(max_length=30)
     hora = models.DateTimeField(auto_now_add=True)
     direccion = models.CharField(max_length=50)
     altura = models.IntegerField()
@@ -20,12 +20,11 @@ class FormPadre(models.Model):
     class Meta:
 
         abstract = True
-        verbose_name = 'FormPadre'
-        verbose_name_plural = 'FormsPadres'
+        verbose_name = 'Formulario Padre'
+        verbose_name_plural = 'Formularios Padres'
 
     def __str__(self):
         return str(self.idAlarma)
-        pass
 
 
 class AccidenteVehicular(FormPadre):
@@ -39,8 +38,8 @@ class AccidenteVehicular(FormPadre):
 
     class Meta:
 
-        verbose_name = 'FormPadre' 
-        verbose_name_plural = 'FormsPadres'
+        verbose_name = 'Accidente Vehicular' 
+        verbose_name_plural = 'Accidentes Vehiculares'
 
     def __str__(self):
         return super().__str__()
@@ -78,8 +77,7 @@ class IncendioVivienda(FormPadre):
     estadoFuego = models.CharField(max_length=30)
     habitantes = models.CharField(max_length=20)
     localHabit = models.CharField(max_length=40) 
-    pisos = models.IntegerField()
-    material = models.CharField(max_length=30)
+    descVivienda = models.CharField(max_length=50)
     espera = models.BooleanField()
     class Meta:
 
@@ -91,14 +89,64 @@ class IncendioVivienda(FormPadre):
 
 class IncendioVehicular(FormPadre):
 
+    tipoVehiculo = models.CharField(max_length=20)
     humoLlamas = models.CharField(max_length=30)
     localOcup = models.BooleanField()
     gnc = models.CharField(max_length=20)
     riesgoProp = models.BooleanField()
     class Meta:
 
-        verbose_name = 'Incendio de Baldio'
-        verbose_name_plural = 'Incendios de Baldios'
+        verbose_name = 'Incendio Vehicular'
+        verbose_name_plural = 'Incendios Vehiculares'
+
+    def __str__(self):
+        return super().__str__()
+
+class IncendioElectrico(FormPadre):
+
+    desperfecto = models.CharField(max_length=30)
+    humoLlamas = models.CharField(max_length=30)
+    epec = models.CharField(max_length=30)
+    servEmergencia = models.CharField(max_length=30)
+    class Meta:
+
+        verbose_name = 'Incendio Electrico'
+        verbose_name_plural = 'Incendios Electricos'
+
+    def __str__(self):
+        return super().__str__()
+
+
+class RescateAnimal(FormPadre):
+
+    animal = models.CharField(max_length=15)
+    condicionAnimal = models.CharField(max_length=30)
+    vision = models.BooleanField()
+
+    class Meta:
+
+        verbose_name = 'Rescate Animal'
+        verbose_name_plural = 'Rescate de Animales'
+
+    def __str__(self):
+        return super().__str__()
+
+class RescateCadaver(FormPadre):
+
+    #* Datos diferentes dependiendo si el que llamos es policia o civil
+    cantPolicia: models.IntegerField()
+    motivoMuerte: models.CharField(max_length=30)
+    estadoCadaver : models.CharField(max_length=35)
+    #* var estadoCadaver incluye si esta descompuesto, localizacion del cuerpo y la posicion
+    #* Datos exclusivos para la Policia:
+    certDefuncion: models.BooleanField()
+    remocion: models.CharField(max_length=30)
+    seguroBomb: models.BooleanField()
+
+    class Meta:
+
+        verbose_name = 'Rescate de Cadaver'
+        verbose_name_plural = 'Rescates de Cadaveres'
 
     def __str__(self):
         return super().__str__()
