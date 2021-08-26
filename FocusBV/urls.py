f53e4beb5ad24de3b3507c8f292361fe50b832da
Lucio <lucio@rakuzanx.sinapsis>
2021-08-26 12:00:14 -0300

    Print form function added

--- FocusBV/urls.py
+++ FocusBV/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('av/',views.ACview , name = "AC"),
     path('if/',views.IFview , name = "IF"),
     path('ra/',views.RAview , name = "RA"),
+    path('ra/pdf/',RAasPDF.as_view()),
     path('ib/',views.IBview , name = "IB"),
     path('ivh/',views.IVhview , name="IVh"),
     path('iv/',views.IVview , name = "IV"),
--- FocusBV/views.py
+++ FocusBV/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render, redirect, get_object_or_404
-import time
-
+# import time,io
 from django.views.decorators.csrf import csrf_protect
-
 from .models import *
 from .forms import *
+from .utils import *
+from django.template.loader import get_template
+from django.views.generic import View
 
 # Create your views here.
 def homeview(request):
@@ -13,7 +14,7 @@ def homeview(request):
     AVf = AVform() # Accidente Vehicular
     IFf = IFform() # Incendio Forestal
     IBf = IBform() # Incendio Baldio
-    IVf = IVform() # Invencio Vivienda
+    IVf = IVform() # Incendio Vivienda
     IVhf = IVhform() # Incendio Vehicular
     IEf = IEform() # Incendio Electrico
     RAf = RAform() # Rescate Animal
@@ -91,12 +92,52 @@ def RAview(request):
         if RAf.is_valid():
             RAf.save(request)
             print("se guardo")
+            # request.session['nombre'] = RAf.cleaned_data['nombre']
+            # request.session['animal'] = RAf.cleaned_data['animal']
+            # request.session['telefono'] = RAf.cleaned_data['telefono']
+            # request.session['direccion'] = RAf.cleaned_data['direccion']
+            # request.session['referencia'] = RAf.cleaned_data['referencia']
+            # request.session['condicion'] = RAf.cleaned_data['condicionAnimal']
 
+            request.session['dict'] = dict = {
+                'nombre': RAf.cleaned_data['nombre'],
+                'animal': RAf.cleaned_data['animal'],
+                'telefono': RAf.cleaned_data['telefono'],
+                'direccion': RAf.cleaned_data['direccion'],
+                'referencia': RAf.cleaned_data['referencia'],
+                'condicion': RAf.cleaned_data['condicionAnimal']
+            }
+
+            return redirect('pdf/',RAf)
+    
     context = {
         'form': RAf,
-    }
-
+    }        
+    
     return render(request,"RAf.html",context)
+
+class RAasPDF(View):
+    def get(self,request,*args,**kwargs):
+        dict = request.session.get('dict')
+        # nombre = request.session.get('nombre')
+        # animal = request.session.get('animal')
+        # telefono = request.session.get('telefono')
+        # direccion = request.session.get('direccion')
+        # referencia = request.session.get('referencia')
+        # condicion = request.session.get('condicion')
+
+        context = {
+            'nombre': dict['nombre'],
+            'animal': dict['animal'],
+            'telefono': dict['telefono'],
+            'direccion': dict['direccion'],
+            'referencia': dict['referencia'],
+            'condicion': dict['condicion'],
+        }
+        # html = template.render(context)
+        pdf = render_to_pdf('RApdf.html', context)
+        return HttpResponse(pdf,content_type='application/pdf')
+
 def IVview(request):
     IVf = IVform()
     if request.method == "POST":
