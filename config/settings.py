4792174de13aa4eb67b3823d69539eaae191dba4
LucasPasolli <lippasolli@hotmail.com>
2021-09-16 10:42:18 -0300

    Implementacion de mysql client, cambios en requirements

--- config/settings.py
+++ config/settings.py
@@ -81,8 +81,12 @@ WSGI_APPLICATION ='config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'focusBV',
+        'USER': 'root',
+        'PASSWORD': 'password',
+        'HOST': '127.0.0.1',
+        'PORT': '',
     }
 }
 
